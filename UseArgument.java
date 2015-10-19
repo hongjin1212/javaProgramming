549a4532b181f5a830675c1df54fc4b89c8863dc
hongjin1212 <hong.jin@duke.edu>
2015-10-19 13:55:21 -0400

    Revised UseArgument

--- UseArgument.java
+++ UseArgument.java
@@ -1,7 +1,7 @@
 public class UseArgument {
     public static void main(String[] args) {
 	System.out.print("Hi, ");
-	System.out.print(args[0]);
-	System.out.println(". How are you?");
+	System.out.println(args[2] + ", " + args[1] + ", and " + args[0]);
+	//System.out.println(". How are you?");
     }
 }
